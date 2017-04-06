9c1f2608b60a7557447ff60695beaa0ee5483ee0
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-07 07:10:17 +0800

    First Commit

---
+++ webdevtproject/wsgi.py
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+"""
+WSGI config for webdevtproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webdevtproject.settings")
+
+application = get_wsgi_application()
+=======
+"""
+WSGI config for webdevtproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webdevtproject.settings")
+
+application = get_wsgi_application()
+>>>>>>> 978b43a3aec7ff7ecf08bc89203c7e8d6121294e
