36030e81fde83dad0ba705cbc64b0655ec7a5821
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-20 23:41:28 +0800

    Modified urls.py

--- webdevtproject/urls.py
+++ webdevtproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import jobportal
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^jobportal/' jobportal.site.urls),
 ]
