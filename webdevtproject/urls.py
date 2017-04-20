bc91a023b0c1c9ff013fd59038ef8c811300e2cb
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-21 02:41:54 +0800

    Modifie webdevtproject/urls.py

--- webdevtproject/urls.py
+++ webdevtproject/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from accounts import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
