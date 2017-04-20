d69bb67a6835729038b6128ea2cee4488fff78a7
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-21 02:39:38 +0800

    Modified webdevtproject/urls.py

--- webdevtproject/urls.py
+++ webdevtproject/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^login$', views.login, name='login'),
     url(r'^admin/', admin.site.urls),
     url(r'^jobportal/', include('jobportal.urls')),
 ]
