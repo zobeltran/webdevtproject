3e56053871e7feccef918582feca10b07dfda242
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-20 23:41:01 +0800

    Modified settings.py

--- webdevtproject/settings.py
+++ webdevtproject/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'webdevtproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dynamics',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
     }
 }
 
