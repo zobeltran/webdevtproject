3d901c5b9ea325031a8e5e364e25542e03280cde
Renzo Beltran <renzoe.beltran@gmail.com>
2017-04-21 02:58:05 +0800

    Turned off debug

--- webdevtproject/settings.py
+++ webdevtproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p!q(*=gp483-5mlb_zvp)wrm=nlnq7@9sitsg+c7ap88j$q)2y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['webdevt.herokuapp.com']
 
